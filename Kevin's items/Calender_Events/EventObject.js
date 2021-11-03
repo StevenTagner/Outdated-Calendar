59e81414b7cbb87c1e18da4c9b07b1f05c05f310
Kevin Hayes <Nivek694@gmail.com>
2021-11-03 16:23:34 -0400

    Worked on edit. Need to talk to team about implementing view()

--- Kevin's items/Calender_Events/EventObject.js
+++ Kevin's items/Calender_Events/EventObject.js
@@ -12,13 +12,13 @@ class EventObject {
 
     /**
      * Constructs an EventObject with the given proproties
-     * @param {Date} startTime
-     * @param {Date} endTime
-     * @param {string} title
-     * @param {string} description
-     * @param {string} creator
-     * @param {Set<string>} attendees
-     * @param {Set<string>} tags
+     * @param {Date} startTime The time the event will start
+     * @param {Date} endTime The time the event will end
+     * @param {string} title The title of the event
+     * @param {string} description A description of the event
+     * @param {string} creator The email address of the creator of the event
+     * @param {Set<string>} attendees A set of strings of the emails of the people who will attend the event.
+     * @param {Set<string>} tags The tags that are attached to the event.
      */
     constructor(startTime = Date.now, endTime = Date.now, title = "Event", description = "an event", creator = "Person", attendees = new Set(), tags = new Set()) {
         this.startTime = startTime;
@@ -35,36 +35,41 @@ class EventObject {
     }
 
     /**
-     * Changes given fields to
-     * @param {Date} startTime
-     * @param {Date} endTime
-     * @param {string} title
-     * @param {string} description
-     * @param {string} creator
-     * @param {Set<string>} attendees
-     * @param {Set<string>} tags
+     * Changes given fields to what is given.
+     * @param {Date} startTime The time the event will start
+     * @param {Date} endTime The time the event will end
+     * @param {string} title The title of the event
+     * @param {string} description A description of the event
+     * @param {string} creator The email address of the creator of the event
+     * @param {Set<string>} attendees A set of strings of the emails of the people who will attend the event.
+     * @param {Set<string>} tags The tags that are attached to the event.
      */
     edit(startTime = null, endTime = null, title = null, description = null, creator = null, attendees = null, tags = null){
         if (startTime != null) {
             this.startTime = startTime;
         }
         if (endTime != null) {
-
+            this.endTime = endTime;
         }
         if (title != null) {
-
+            this.title = title;
         }
         if (description != null) {
-
+            this.description = description;
         }
         if (creator != null) {
-
+            this.creator = creator;
         }
         if (attendees != null) {
-
+            this.attendees = attendees;
         }
         if (tags != null) {
-
+            this.tags = tags;
         }
+    }
+
+    //TODO: ask how we should handle view.
+    view() {
+        throw "Method not implemented yet";
     }
 }
