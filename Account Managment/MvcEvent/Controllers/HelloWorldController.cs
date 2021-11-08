d756a02b603f8d4b9f16b789bfe3a4eb988b4720
Steven <StevenT1616@gmail.com>
2021-11-08 12:59:31 -0500

    First Iteration of Event Controller/Web App
    Basic controller takes in a event name and ID, will continue Wed.

---
+++ Account Managment/MvcEvent/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Event: {name}, ID: {ID}");
+        }
+    }
+}
