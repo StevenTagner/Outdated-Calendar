7e95fd26d1e8f609c676e94e55bfec9e52309055
Steven <StevenT1616@gmail.com>
2021-11-10 13:05:29 -0500

    Adding Views
    Added some views + controllers to allow you to modify what info you display on the site at any given time.

--- Account Managment/MvcEvent/Controllers/HelloWorldController.cs
+++ Account Managment/MvcEvent/Controllers/HelloWorldController.cs
@@ -5,17 +5,17 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // 
-        // GET: /HelloWorld/
-
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
-        public string Welcome(string name, int ID = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Event: {name}, ID: {ID}");
+            ViewData["Message"] = "test  " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
