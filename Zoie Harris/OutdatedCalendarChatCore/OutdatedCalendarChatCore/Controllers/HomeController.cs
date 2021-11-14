0fccd9369f72df4b9850d551e96effb673bc6706
Zoie Harris <zharris6866@gmail.com>
2021-11-14 13:56:55 -0500

    updated database. presented on website view

--- Zoie Harris/OutdatedCalendarChatCore/OutdatedCalendarChatCore/Controllers/HomeController.cs
+++ Zoie Harris/OutdatedCalendarChatCore/OutdatedCalendarChatCore/Controllers/HomeController.cs
@@ -4,7 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
+using OutdatedCalendarchat.Models;
 using OutdatedCalendarChatCore.Models;
 
 namespace OutdatedCalendarChatCore.Controllers
@@ -17,7 +19,35 @@ namespace OutdatedCalendarChatCore.Controllers
         {
             _logger = logger;
         }
+        public ActionResult AllMessages()
+        {
+            var mvcName = typeof(Controller).Assembly.GetName();
+            var isMono = Type.GetType("Mono.Runtime") != null;
 
+            ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+            ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+            List<MessageViewModel> messages = new List<MessageViewModel>();
+            var testMessage = new MessageViewModel
+            {
+                From = "Chuck",
+                To = "Zoie",
+                Desc = "This is a test",
+                SentDate = DateTime.Now
+            };
+            messages.Add(testMessage);
+            ViewBag.Messages = messages;
+            return View();
+        }
+        public ActionResult SendMessage()
+        {
+            var sendMessage = new SendMessageViewModel();
+            List<RecipientViewModel> recipients = new List<RecipientViewModel>();
+            recipients.Add(new RecipientViewModel { Name = "Chuck" });
+            recipients.Add(new RecipientViewModel { Name = "Tom" });
+            recipients.Add(new RecipientViewModel { Name = "John" });
+            sendMessage.AvailableRecipientList = new MultiSelectList(recipients, "Id", "Name");
+            return View(sendMessage);
+        }
         public IActionResult Index()
         {
             return View();
---
+++ Zoie Harris/OutdatedCalendarChatCore/OutdatedCalendarChatCore/ViewModels/SendMessageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+
+
+namespace OutdatedCalendarchat.Models
+{
+    public class SendMessageViewModel
+    {
+        public MultiSelectList AvailableRecipientList { get; set; }
+        public List<int> SelectedRecipientList { get; set; }
+        public string Desc { get; set; }
+    }
+}
