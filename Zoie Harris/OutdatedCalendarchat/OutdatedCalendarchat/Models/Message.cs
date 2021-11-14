857411eb0c6f9d1ae2223c1553367ef373da5056
Zoie Harris <Zharris6866@gmail.com>
2021-11-13 21:11:52 -0500

    updated chat stuff

---
+++ Zoie Harris/OutdatedCalendarchat/OutdatedCalendarchat/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace OutdatedCalendarchat.Models
+{
+    public class Message
+    {
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public string Desc { get; set; }
+        public DateTime SentDate { get; set; }
+    }
+}
---
+++ Zoie Harris/OutdatedCalendarchat/OutdatedCalendarchat/Models/SendMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OutdatedCalendarchat.Models
+{
+    public class SendMessage
+    {
+        public List<string> AvailableRecipientList { get; set; }
+        public List<string> SelectedRecipientList { get; set; }
+        public string Desc { get; set; }
+    }
+}
